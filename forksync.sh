66960756a5d5f7dffef7339e65d45725e7fd367a
Akshay Vaidya <akshayvaidya23@gmail.com>
2016-03-25 01:06:16 +0530

    check if git installation present for fork auto sync

--- forksync.sh
+++ forksync.sh
@@ -1,5 +1,14 @@
-git reset --hard
-git checkout master
-git fetch upstream
-git rebase upstream/master
-git push origin master
+#!/bin/sh
+GIT_INSTALLED_CHECK="$(which -a git)"
+if [[ -nz "$GIT_INSTALLED_CHECK" ]]
+  then
+  echo "Beginning fork sync"
+  git reset --hard
+  git checkout master
+  git fetch upstream
+  git rebase upstream/master
+  git push origin master
+  echo "Fork sync complete"
+else
+  echo "git not installed"
+fi
